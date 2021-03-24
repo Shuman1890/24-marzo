d76f158752c80b1c3cc9e0b4c7a817d7adab7a62
andrescortesdev <andrescortes.development@gmail.com>
2021-03-24 19:45:23 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quibusdam?</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores autem distinctio dolorum eveniet
+                labore minima molestiae nostrum pariatur quasi, reprehenderit rerum, sapiente sit unde. Excepturi
+                nesciunt praesentium quis reiciendis rem?
+            </p>
+            <button class="btn btn-primary">Button</button>
+            <br>
+              <i>Robinson Andres Cortes: KEY</i>
+        </div>
+    </div>
+</div>
+</body>
+</html>
